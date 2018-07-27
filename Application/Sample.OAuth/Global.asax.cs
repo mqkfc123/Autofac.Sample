83ffc4070336520c8ad3ba315b4ca901024fa41a
liuyl <mqkfc123@163.com>
2018-07-27 23:39:15 +0800

    新增配置

--- Application/Sample.OAuth/Global.asax.cs
+++ Application/Sample.OAuth/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using Dragon.Core;
 
 namespace Sample.OAuth
 {
@@ -14,10 +15,15 @@ namespace Sample.OAuth
     {
         void Application_Start(object sender, EventArgs e)
         {
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
+
             // 在应用程序启动时运行的代码
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var coreBuilder = new CoreBuilder();
+            coreBuilder.Build();
         }
     }
 }
